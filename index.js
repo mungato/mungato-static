dd1180db3501c2ff39ed0037840039aafa21c101
amkayondo <amkayondo01@gmail.com>
2021-08-12 19:56:09 +0300

    add initial static files :)

---
+++ index.js
@@ -0,0 +1,16 @@
+const StaticServer = require("static-server");
+
+const server = new StaticServer({
+  rootPath: ".",
+  port: 9080,
+  cors: "*",
+  followSymlink: true,
+});
+
+server.cors('Access-Control-Allow-Origin', 'localhost')
+
+server.start(function () {
+  console.log(`Mungato static server run on PORT ${server.port}`);
+});
+
+
